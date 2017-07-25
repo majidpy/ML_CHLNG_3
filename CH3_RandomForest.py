2a99678292a98bd497fc67ab56db5417272bf64e
mojtaba-komeili <mojtaba.komeili@gmail.com>
2017-07-24 23:59:11 -0600

    Added Random Forest

--- CH3_RandomForest.py
+++ CH3_RandomForest.py
@@ -2,14 +2,14 @@
 A script for training of HackerEarth CH3 data
 """
 from CH3_data_loader import load_training_data, load_test_data, save_test_results
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 ###############     Constants     ###############
 DEBUG_MODE = False 
 DROP_NAN = False
 SPLIT_FRACTION = 0.0
 RUN_MAIN_TEST = True
-SAVE_FILE_NAME = 'results_dec_tree.csv'
+SAVE_FILE_NAME = 'results_rand_forest.csv'
 
 ###############     Training     ###############
 data = load_training_data(split_frac=SPLIT_FRACTION, 
@@ -21,13 +21,13 @@ y_train = data[2]
 y_test  = data[3]
 
 print('Started training ...\n')
-dtc = tree.DecisionTreeClassifier()
-dtc_model = dtc.fit(X_train, y_train)
+rfc = RandomForestClassifier(n_jobs=2) # Running 2 on cores
+rfc_model = rfc.fit(X_train, y_train)
 print('Training finished.\n')
 
 if SPLIT_FRACTION > 0:
     print('Started running cross validation ...\n')
-    pred_score = dtc_model.score(X_test, y_test)
+    pred_score = rfc_model.score(X_test, y_test)
     print('Finished running cross validation.\n')
     print('predicted score on the cross validation set is %.4f' %(pred_score*100))
 
@@ -35,7 +35,7 @@ if SPLIT_FRACTION > 0:
 if RUN_MAIN_TEST:
     X, ID = load_test_data(drop_na=False, testing_mode=DEBUG_MODE)
     print('Started running the main test ...')
-    y = dtc_model.predict_proba(X)
+    y = rfc_model.predict_proba(X)
     print('Finished running the main test.')
     save_test_results(ID, y[:,1], SAVE_FILE_NAME)
     print('Saved results to \"data\\%s\"' %SAVE_FILE_NAME)
