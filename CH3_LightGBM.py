b40c5d552b7dbb58ed80e23d24abfeb0a5fa6eff
mojtaba-komeili <mojtaba.komeili@gmail.com>
2017-07-28 17:54:56 -0600

    Adding LightGBM method

---
+++ CH3_LightGBM.py
@@ -0,0 +1,51 @@
+"""
+A script for training of HackerEarth CH3 data
+"""
+from CH3_data_loader import load_training_data, load_test_data, save_test_results
+import lightgbm as lgb
+from sklearn import metrics
+
+###############     Constants     ###############
+DEBUG_MODE = False 
+DROP_NAN = False
+SPLIT_FRACTION = 0.2
+RUN_MAIN_TEST = True
+SAVE_FILE_NAME = 'results_light_gbm.csv'
+
+###############     Training     ###############
+data = load_training_data(split_frac=SPLIT_FRACTION, 
+                        drop_na=DROP_NAN, testing_mode=DEBUG_MODE)
+
+X_train = data[0]
+X_test  = data[1] 
+y_train = data[2] 
+y_test  = data[3]
+training_encoder = data[4]
+
+print('Started training ...\n')
+dtrain = lgb.Dataset(X_train, y_train)
+dval = lgb.Dataset(X_test, y_test, reference=dtrain)
+params = {
+    'num_leaves' : 20,
+    'learning_rate':0.01,
+    'metric':'auc',
+    'application':'binary',
+    'early_stopping_round': 40,
+    'max_depth':40,
+    'bagging_fraction':0.5,
+    'feature_fraction':0.6,
+    'verbose' : 1  
+}
+num_rounds = 10
+lgb_model = lgb.train(params, dtrain, num_rounds, valid_sets=dval)
+print('Training finished.\n')
+
+###############     Testing main data     ###############    
+if RUN_MAIN_TEST:
+    X, ID = load_test_data(training_encoder, 
+                           drop_na=False, testing_mode=DEBUG_MODE)
+    print('Started running the main test ...')
+    y = lgb_model.predict(X)
+    print('Finished running the main test.')
+    save_test_results(ID, y, SAVE_FILE_NAME)
+    print('Saved results to \"data\\%s\"' %SAVE_FILE_NAME)
